a01bf09237d09544e6b1d5f5ed302f5a9660c466
Okoth Trevor <oktrevoro@gmail.com>
2022-03-29 12:12:36 -0700

    Done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum += a[(size * i) + i];
-		sum1 += a[(size * (i = 1)) - (i + 1)];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
 	}
 
 	printf("%d, %d\n", sum, sum1);
