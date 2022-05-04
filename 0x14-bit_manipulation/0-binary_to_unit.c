2d193faa0ce5e1645bfb5217b85b4bce90a6894f
Okoth Trevor <oktrevoro@gmail.com>
2022-05-04 10:35:44 -0700

    Hi bitwise

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * binary_to_unit - function that converts binary to unsigned int
+ * binary_to_uint - function that converts binary to usigned int
  * @b: string containing the binary number
  * Return: the converted number
  */
-unsigned int binary_to_unit(const char *b)
+
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int decimal_value = 0;
@@ -16,7 +17,7 @@ unsigned int binary_to_unit(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decimal_value = 2 * decimal_value +(b[i] - '0');
+		decimal_value = 2 * decimal_value + (b[i] - '0');
 	}
 
 	return (decimal_value);
