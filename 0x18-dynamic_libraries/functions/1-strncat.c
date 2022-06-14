fa695dbb9e9c1dda1c1f5c34bcaa4797d065c0c5
OKOTH TREVOR <95345665+OkayTrevor@users.noreply.github.com>
2022-06-14 17:30:27 +0300

    1-strcat

--- 0x18-dynamic_libraries/functions/1-strncat.c
+++ 0x18-dynamic_libraries/functions/1-strncat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * *_strncat - concatenates n bytes of two strings
  * @dest: pointer destination
@@ -10,15 +10,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
 ;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
-
-/*should end with a end of string char*/
-dest[dest_len + i] = '\0';
+for (j = 0; src[j] != '\0' && n > 0;j++, n--, i++)
+	dest[i] = src[j];
 
 return (dest);
 }
