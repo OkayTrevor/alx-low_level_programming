f77bf3bca71eaa0e7cad93a6b39b1999c742ee1b
OKOTH TREVOR <95345665+OkayTrevor@users.noreply.github.com>
2022-06-14 17:16:09 +0300

    strcat

--- 0x18-dynamic_libraries/functions/0-strcat.c
+++ 0x18-dynamic_libraries/functions/0-strcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * *_strcat - concatenates two strings
  * @dest: pointer destination
@@ -8,15 +8,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
+int i, j;
 for (i = 0; dest[i] != '\0'; i++)
 ;
-
-do {
-	a++;
-	dest[i] = src[a];
+for (j = 0; src[j] != '\0'; j++)
+{
+	dest[i] = src[j];
 	i++;
-} while (src[a] != '\0');
-
-return (dest);
+} 
+	dest[i] = '\0';
+	return (dest);
 }
