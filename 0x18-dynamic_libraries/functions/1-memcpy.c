9e5b6d2e076758e18d33658bd66f740c725d1d5c
OKOTH TREVOR <95345665+OkayTrevor@users.noreply.github.com>
2022-06-14 17:09:18 +0300

    memcpy

--- 0x18-dynamic_libraries/functions/1-memcpy.c
+++ 0x18-dynamic_libraries/functions/1-memcpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * *_memcpy -  copies memory area from src to dest
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0 ; i++, n--)
+	for (i = 0; i < n; i++,)
 	{
 		dest[i] = src[i];
 	}
